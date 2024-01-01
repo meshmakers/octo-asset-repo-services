23d79349b8419740059ac356e316df8507fc4043
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-01-01 22:00:38 +0100

    Updated submodules

---
+++ src/AssetRepositoryServices/GraphQL/Types/RtEnumScalarType.cs
@@ -0,0 +1,46 @@
+using GraphQL;
+using GraphQL.DataLoader;
+using GraphQL.Types;
+using Meshmakers.Octo.Backend.AssetRepositoryServices.GraphQL.Caches;
+using Meshmakers.Octo.Backend.AssetRepositoryServices.GraphQL.RequestHandling;
+using Meshmakers.Octo.ConstructionKit.Contracts;
+using Meshmakers.Octo.ConstructionKit.Contracts.DependencyGraph;
+using Meshmakers.Octo.ConstructionKit.Contracts.Services;
+
+#pragma warning disable 1591
+
+namespace Meshmakers.Octo.Backend.AssetRepositoryServices.GraphQL.Types;
+
+public class RtEnumType : EnumerationGraphType
+{
+    public CkId<CkEnumId> CkEnumId { get; }
+
+    public RtEnumType(CkId<CkEnumId> ckEnumId)
+    {
+        CkEnumId = ckEnumId;
+        Name = ckEnumId.GetGraphQlName();
+        Description = $"Runtime entities of construction kit enum '{ckEnumId}'";
+    }
+    
+    internal void Populate(ICkCacheService ckCacheService, string tenantId, IGraphTypesCache graphTypesCache,
+        IDataLoaderContextAccessor dataLoaderAccessor,
+        IOctoSessionAccessor sessionAccessor, CkEnumGraph ckEnumGraph)
+    {
+        var enumGraphData = ckEnumGraph.Values.Select(e => (
+            name: e.Name.ToConstantCase(),
+            value: e.Key,
+            description: e.Description
+        ));
+        
+        foreach (var (name, value, description) in enumGraphData)
+        {
+            var enumValue = new EnumValueDefinition(name, value)
+            {
+                Description = description
+            };
+            
+            Add(enumValue);
+        }
+    } 
+    
+}
