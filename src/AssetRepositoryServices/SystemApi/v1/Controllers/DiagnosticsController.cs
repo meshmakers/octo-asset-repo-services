a82de91b5f11773209530352be75bf36a089cccf
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-08-30 12:10:19 +0200

    AB#1406: New: Allow configuration of log level at runtime

--- src/AssetRepositoryServices/SystemApi/v1/Controllers/DiagnosticsController.cs
+++ src/AssetRepositoryServices/SystemApi/v1/Controllers/DiagnosticsController.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Meshmakers.Octo.Communication.Contracts.DataTransferObjects;
 using Meshmakers.Octo.Services.Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Meshmakers.Octo.Backend.AssetRepositoryServices.SystemApi.v1.Controllers;
